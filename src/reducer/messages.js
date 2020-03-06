e5a8d87acfc0b339013a9f4181ad50d6006148d5
manchandajayant <maanjayant@gmail.com>
2020-03-06 14:48:41 +0100

    case statement for a payload created in reducer to update the  state

--- src/reducer/messages.js
+++ src/reducer/messages.js
@@ -1,6 +1,10 @@
 //reducer takes 2 arguments, state of the container and an action
 function reducer(state = [], action) {
   switch (action.type) {
+    case "ALL_MESSAGES": {
+      console.log("payload", action.payload); //check your state in the  redux store at this step
+      return action.payload;
+    }
     default:
       return state;
   }
