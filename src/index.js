13e4f66d9cebe97b3408aec92a7306e49fa9d6d4
manchandajayant <maanjayant@gmail.com>
2020-03-06 11:36:16 +0100

    store and reducers created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./store";
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//pass the store as a prop to app
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducer/messages.js
@@ -0,0 +1,9 @@
+//reducer takes 2 arguments, state of the container and an action
+function reducer(state = [], action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
