fd88f31b0f14b1e0cb2ed0f06135627956a5d6bb
manchandajayant <maanjayant@gmail.com>
2020-03-06 12:06:01 +0100

    connected to react page and a form input made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
+import superagent from "superagent";
 
 class App extends React.Component {
+  onSubmit = async event => {
+    event.preventDefault();
+    try {
+      const res = await superagent
+        .post("http://localhost:4000/message")
+        .send({ text: "Hardcoded" });
+
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   render() {
-    return <div></div>;
+    return (
+      <div>
+        Hello!!This app is working
+        <form onSubmit={this.onSubmit}>
+          <input type="text" />
+          <button>Send</button>
+        </form>
+      </div>
+    );
   }
 }
 
