2592299e322f5a54d96982b56657373adfd919f9
manchandajayant <maanjayant@gmail.com>
2020-03-06 12:20:58 +0100

    fully controlled form added to the app with a reset button

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from "react";
 import superagent from "superagent";
 
 class App extends React.Component {
+  state = {
+    text: ""
+  };
+
   onSubmit = async event => {
     event.preventDefault();
     try {
       const res = await superagent
         .post("http://localhost:4000/message")
-        .send({ text: "Hardcoded" });
+        .send({ text: this.state.text });
 
       console.log(res);
     } catch (error) {
       console.error(error);
     }
   };
+
+  onChange = event => {
+    console.log("text input made", event.target.value);
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      text: ""
+    });
+  };
   render() {
     return (
       <div>
         Hello!!This app is working
         <form onSubmit={this.onSubmit}>
-          <input type="text" />
+          <input type="text" onChange={this.onChange} value={this.state.text} />
           <button>Send</button>
+          <button onClick={this.reset}>Reset</button>
         </form>
       </div>
     );
